27af3eae5e0d191f94c62c120e1e2607e18fdf77
Frodo B <frodo@mordor.com>
2026-08-24 18:24:00 +0100

    first commit

---
+++ src/main/java/com/frodo/glamdring/GlamdringApplication.java
@@ -0,0 +1,13 @@
+package com.frodo.glamdring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlamdringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlamdringApplication.class, args);
+	}
+
+}
