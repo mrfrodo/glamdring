036a8c501a2f3f46da5a72156c19ed8a333c77b6
Frodo B <frodo@mordor.com>
2026-08-24 19:40:13 +0100

    Renamed start class

--- src/main/java/com/frodo/glamdring/Glamdring.java
+++ src/main/java/com/frodo/glamdring/Glamdring.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GlamdringApplication {
+public class Glamdring {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GlamdringApplication.class, args);
+		SpringApplication.run(Glamdring.class, args);
 	}
 
 }
