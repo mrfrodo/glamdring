5cc6b0bd32dfead76339c79e384909e9c0e51c36
Frodo B <frodo@mordor.com>
2026-09-06 05:34:54 +0100

    add k count

--- src/main/java/com/frodo/glamdring/infrastructure/adapters/in/web/HomeController.java
+++ src/main/java/com/frodo/glamdring/infrastructure/adapters/in/web/HomeController.java
@@ -1,5 +1,6 @@
 package com.frodo.glamdring.infrastructure.adapters.in.web;
 
+import com.frodo.glamdring.application.ports.in.GetKillLogUseCase;
 import com.frodo.glamdring.application.ports.in.GetTechUseCase;
 import com.frodo.glamdring.application.ports.in.GetTipOfTheDayUseCase;
 import org.springframework.stereotype.Controller;
@@ -13,16 +14,19 @@ public class HomeController {
 
     private final GetTechUseCase getTechUseCase;
     private final GetTipOfTheDayUseCase getTipOfTheDayUseCase;
+    private final GetKillLogUseCase getKillLogUseCase;
 
-    public HomeController(GetTechUseCase getTechUseCase, GetTipOfTheDayUseCase getTipOfTheDayUseCase) {
+    public HomeController(GetTechUseCase getTechUseCase, GetTipOfTheDayUseCase getTipOfTheDayUseCase, GetKillLogUseCase getKillLogUseCase) {
         this.getTechUseCase = getTechUseCase;
         this.getTipOfTheDayUseCase = getTipOfTheDayUseCase;
+        this.getKillLogUseCase = getKillLogUseCase;
     }
 
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("trends", getTechUseCase.getTopTrends(INITIAL_TREND_LIMIT));
         model.addAttribute("tipOfTheDay", getTipOfTheDayUseCase.getTip().orElse(null));
+        model.addAttribute("killCount", getKillLogUseCase.getKills().size());
         return "index";
     }
 }
